6710279e51bb71bbcdc1bc0e7da8873d414871a3
Jonathan Azoff <jon@azoffdesign.com>
2012-05-27 15:33:18 -0700

    added html loading mechanism

--- src/tacion.js
+++ src/tacion.js
@@ -1,20 +1,95 @@
-(function(global, dom, SocketApi, mobile, $){
+(function(global, dom, loader, SocketApi, mobile, $){
     
-    var root, content, folder;
-    var state = {};
+    var hidden = 'ui-hidden-accessible';
+    var page, content, header, footer;
+    var urlcache = {}, folder;
+    var state = { };
     
-    function render(html, data) {
-        return ;
+    function spinner(msgText) {
+        if (msgText) {
+            mobile.showPageLoadingMsg('a', msgText, true);
+        } else {
+            mobile.hidePageLoadingMsg();
+        }
     }
     
-    function setSlide(data) {
-        if (state.slide <= state.slides.length) {
-            var path = state.slides[state.slide];
-            var url = folder + '/' + path;
-            $.get(url).then(function(html) {
-                render(html, data);
-            });
+    function renderslide(html) {
+        var job = $.Deferred();
+        var slide = $(html);
+        var title = slide.find('title').html();
+        var article = slide.find('article');
+        var assets = slide.find('link[href],script[src]').remove();
+        var done = function(){ job.resolve(article); }
+        var urls = $.makeArray(assets.map(function(){
+            // don't process URLs we've already downloaded
+            var asset = $(this);
+            var url = asset.attr('src') || asset.attr('href');
+            if (url in urlcache) { return; } 
+            else { return folder + '/' + url; }
+        }));   
+        article.addClass(hidden);     
+        article.appendTo(content).data('title', title);
+        if (urls.length) {
+            loader({ load: urls, complete: done() });
+        } else {
+            done();
         }
+        return job.promise();
+    }
+    
+    function loadslide(path) {
+        var url = folder + '/' + path;
+        var job = $.Deferred();
+        $.get(url).then(function(html) {
+             renderslide(html).then(job.resolve);
+        });
+        return job.promise();
+    }
+    
+    function getslide(index) {
+        var job = $.Deferred();
+        var slide = state.slides[index];        
+        if ($.type(slide) === 'string') {
+            state.slides[index] = 
+                loadslide(slide).then(job.resolve);
+        } else {
+            slide.then(job.resolve);
+        }
+        return job.promise();
+    }
+    
+    function gotostep(step, slide) {
+        var classNames = slide.prop('className') || '';
+        var largestStep = 0, stepLength = step+1, i;
+        // only remove steps that we don't need
+        slide.prop('className', classNames.replace(/step-(\d+)/g, 
+            function(className, stepName){    
+                var currentStep = parseInt(stepName, 10);
+                largestStep = currentStep > largestStep ? currentStep : largestStep;
+                if (currentStep > step) { return ''; } 
+                else { return className; }
+            })
+        );
+        for (i=largestStep; i<stepLength; i++) {
+            slide.addClass('step-' + i);
+        }
+    }
+    
+    function update(index, step, data) {
+        spinner('loading slide');
+        getslide(index).then(function(slide){                        
+            if (state.slide !== index) {                
+                slide.siblings().addClass(hidden);
+                slide.removeClass(hidden);
+                state.slide = index;
+                header.html(slide.data('title'));
+                footer.html((index+1) + ' of ' + state.count)
+            }
+            state.step = step;
+            gotostep(step, slide);
+            mobile.changePage(page.page(), data);           
+            spinner(false); 
+        });
     }
 
     function urlstate(href) {
@@ -30,25 +105,21 @@
     }
 
     function unsigned(i) {
-        return i !== undefined && i > 0;
+        return $.type(i) === 'number' && i > 0;
     }
 
     function change(step, slide) {        
-        step = unsigned(step) ? step : state.step;
-        slide = unsigned(slide) ? slide : state.slide;
-        mobile.changePage('#slide='+slide+'step='+step);
+        step = unsigned(step) ? step : (unsigned(state.step) ? state.step : 0);
+        slide = unsigned(slide) ? slide : (unsigned(state.slide) ? state.slide : 0);
+        mobile.changePage('#slide='+slide+'&step='+step);
     }
 
-    function onchange(event, data) {
+    function onchange(event, data) {    
         var location = data.toPage;
-        if ($.type(location) === 'string') {
+        if ($.type(location) === 'string') {            
             var current = urlstate(location);                  
+            update(current.slide, current.step, data);
             event.preventDefault();
-            if (current.slide !== state.slide) {
-                setSlide(current.slide, data);
-            } else if (current.step !== state.step) {
-                setStep(current.step, data);
-            }
         }
     }
     
@@ -57,7 +128,10 @@
         var current = urlstate();
         page = root.find('#page');
         content = page.find('#content');
+        header = page.find('#header h1');
+        footer = page.find('#footer h1');
         state.slides = manifest.slides;
+        state.count = manifest.slides.length;
         root.on('pagebeforechange', onchange);
         change(current.step, current.slide);
         /*var socketApi = new SocketApi(manifest.apiKey);
@@ -81,16 +155,21 @@
         }, 2000)*/
     }
     
+    function start(presentation) {
+        spinner('loading presentation');
+        folder = presentation;
+        $.getJSON(folder+'/manifest.json').then(init);
+    }
+    
+    // Expose the public API
     global.tacion = {
-        start: function(presentation) {
-            folder = presentation;
-            $.getJSON(folder+'/manifest.json').then(init);
-        }
+        start: start,
+        change: change
     };
     
     
     
-})(window, document, Pusher, jQuery.mobile, jQuery);
+})(window, document, yepnope, Pusher, jQuery.mobile, jQuery);
 
 // Enable pusher logging - don't include this in production
 Pusher.log = function(message) {
